afc96d6fb731009714baa0b02fb0a03ff14bb30e
shubham-Alhat <shubhamalhat520@gmail.com>
2025-08-16 23:48:58 +0530

    22:12  continued - db setup not completed yet

---
+++ src/app/generate-program/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const GenerateProgram = () => {
+  return (
+    <>
+      <div>Generaterogarm</div>
+      <Button>Click me</Button>
+    </>
+  );
+};
+
+export default GenerateProgram;
