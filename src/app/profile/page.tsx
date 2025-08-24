5f9a73941be4541d35114af79d4c34774362e177
shubham-Alhat <shubhamalhat520@gmail.com>
2025-08-24 23:15:18 +0530

    UI

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { auth } from "../../../auth";
+import ProfileHeader from "@/components/ProfileHeader";
 
 const Profile = async () => {
-  // const session = await auth();
+  const session = await auth();
   // console.log(session?.user);
-  return <div>Profile </div>;
+  return (
+    <>
+      <section className="relative z-10 pt-12 pb-32 flex-grow container mx-auto px-4">
+        <ProfileHeader user={session?.user} />
+      </section>
+    </>
+  );
 };
 
 export default Profile;
